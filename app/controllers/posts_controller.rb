7018943b3d164d9faca750c0ff34e8970d7bdb72
Toki Fukumoto <tokifukumoto@TokinoMacBook-Pro.local>
2021-04-08 05:48:07 +0900

    you can see more

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
+  before_action :ensure_correct_user,{only: [:edit, :update, :destroy]}
+
   def index
       @posts = Post.all.order(created_at: :desc)
   end
 
   def show
       @post = Post.find_by(id: params[:id])
-      @user = User.find_by(id: @post.user_id)
+      @user = @post.user
   end
 
   def new
@@ -46,4 +49,12 @@ class PostsController < ApplicationController
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   validates :password,{presence:true}
+  def posts
+    return Post.where(user_id:self.id)
+  end
 end
